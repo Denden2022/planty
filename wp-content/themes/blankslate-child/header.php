12dcda598647f08e45357ac7aa526eaa009b75c6
Denise Fontanier <deniseden.c@gmail.com>
2023-11-10 12:54:18 +0100

    insertion élément page accueil

--- wp-content/themes/blankslate-child/header.php
+++ wp-content/themes/blankslate-child/header.php
@@ -8,7 +8,7 @@
 <header id="header" role="banner">
 <div id="branding">
     <div id="branding-img">
-        <?php echo '<img src="http://localhost/Planty/wp-content/uploads/2023/11/Logo-source.png" alt="logo">'?>
+        <?php echo '<a href="http://localhost/Planty/" ><img src="http://localhost/Planty/wp-content/uploads/2023/11/Logo-source.png" alt="logo"></a>'?>
     </div>
     <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
         <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
---
+++ wp-content/themes/blankslate-child/page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div class="entry-content" itemprop="mainContentOfPage">
+
+<?php the_content(); ?>
+
+</div>
+
+<?php get_footer(); ?>
