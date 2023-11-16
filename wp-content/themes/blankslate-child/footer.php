00b016f51e820e07bc3e37da9bb64066eec5e103
Denise Fontanier <deniseden.c@gmail.com>
2023-11-16 14:52:34 +0100

    mise en page de toutes les pages

--- wp-content/themes/blankslate-child/footer.php
+++ wp-content/themes/blankslate-child/footer.php
@@ -2,7 +2,7 @@
 </main>
 </div>
 <footer id="footer" role="contentinfo">
-<?php echo '<a href="Mentions légales">Mentions légales</a>'; ?>
+<?php echo '<a href="http://localhost/Planty/nous-rencontrer/Mentions-legales">Mentions légales</a>'; ?>
 </footer>
 </div>
 <?php wp_footer(); ?>
