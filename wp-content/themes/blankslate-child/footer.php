b6f2621477a679733f9b4b7d2660ee9e0af331e7
Denise Fontanier <deniseden.c@gmail.com>
2023-12-01 15:24:43 +0100

    projet final P6

--- wp-content/themes/blankslate-child/footer.php
+++ wp-content/themes/blankslate-child/footer.php
@@ -4,7 +4,6 @@
 <footer id="footer" role="contentinfo">
 <?php echo '<a href="http://localhost/Planty/nous-rencontrer/Mentions-legales">Mentions légales</a>'; ?>
 </footer>
-</div>
 <?php wp_footer(); ?>
 </body>
 </html>
